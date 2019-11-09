a19c8c9d3bcc19d20f7ae00eddebe1f2381be4bb
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-11-09 15:28:25 -0500

    first commit

---
+++ src/Char/Char.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const char = () => {
+    const style = {
+        display: 'inline-block',
+        padding: '16px',
+        textalign: 'center',
+        margin: '16px',
+        border: '1px solid black'
+    };
+
+    return(
+        <div className="Char" style={style}>
+        </div>
+    )
+}
+
+export default char;
